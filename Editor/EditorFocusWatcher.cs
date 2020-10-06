ed4e9e4db76dcd507fbbf0b723e863a7793a22dd
baba_s <shotababa1206@gmail.com>
2020-10-06 20:45:48 +0900

    first commit

---
+++ Editor/EditorFocusWatcher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEditor;
+using UnityEditorInternal;
+
+namespace Kogane
+{
+	[InitializeOnLoad]
+	public static class EditorFocusWatcher
+	{
+		public static Action OnFocused   { get; set; }
+		public static Action OnUnfocused { get; set; }
+
+		private static bool m_isFocused;
+
+		static EditorFocusWatcher()
+		{
+			EditorApplication.update += Update;
+		}
+
+		private static void Update()
+		{
+			var isApplicationActive = InternalEditorUtility.isApplicationActive;
+
+			if ( !m_isFocused && isApplicationActive )
+			{
+				m_isFocused = true;
+				OnFocused?.Invoke();
+				;
+			}
+			else if ( m_isFocused && !isApplicationActive )
+			{
+				m_isFocused = false;
+				OnUnfocused?.Invoke();
+			}
+		}
+	}
+}
